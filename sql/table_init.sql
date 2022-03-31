5f83c1a085d61162b77f5dcba8c70137d8f48932
Daníel Örn Árnason <daarn@slagelse.dk>
2022-03-31 15:28:25 +0200

    lytter efter button_clicked og skriver i button_clicks tabel

--- sql/table_init.sql
+++ sql/table_init.sql
@@ -12,3 +12,14 @@ create table sps_analytics.themes_interactions(
 	site varchar(255),
 	profile varchar(255)
 )
+
+create table sps_analytics.button_clicks(
+	session_id varchar(255),
+	"event" varchar(255),
+	"time" timestamp default now(),
+	displayname varchar(255),
+	buttonname varchar(255),
+	buttontype varchar(255),
+	site varchar(255),
+	profile varchar(255)
+)
